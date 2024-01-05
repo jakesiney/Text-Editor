b06a6095de2d9364a7af0067b95a35a10f3f034d
Jake Siney <jakesiney@gmail.com>
2024-01-05 13:56:18 +0000

    Install Tkinter

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import tkinter as tk
 def main():
     window = tk.Tk()
     window.title("Text Editor")
+    window.rowconfigure(0, minsize=400, weight=1)
+    window.columnconfigure(0, minsize=500, weight=1)
+
+    text_edit = tk.Text(window, font=("Helvetica", 18))
+    text_edit.grid(row=0, column=0)
 
     window.mainloop()
 
